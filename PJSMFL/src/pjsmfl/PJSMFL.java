ab381d5f0f11699e6348d0198d03384ec337bb5b
CristianAguerreClavel <Aguerre.clavel.cristian@gmail.com>
2014-04-01 11:02:18 +0200

    Creado proyecto de prueba para JSMFL y actualizacion de diario

---
+++ PJSMFL/src/pjsmfl/PJSMFL.java
@@ -0,0 +1,16 @@
+package pjsmfl;
+
+/**
+ *
+ * @author Develop
+ */
+public class PJSMFL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
