b77c259602141c85b66b99cee87e534f824808b2
CristianAguerreClavel <Aguerre.clavel.cristian@gmail.com>
2014-04-08 09:28:18 +0200

    Actualizacion del reproductor con un thread sencillo

--- PReproductorJavaFx/src/preproductorjavafx/PReproductorJavaFx.java
+++ PReproductorJavaFx/src/preproductorjavafx/PReproductorJavaFx.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
@@ -24,26 +25,43 @@ public class PReproductorJavaFx extends Application{
         launch(args);
     }
   
+    /*
+    *   Creamos la interfaz del reproductor
+    */
     @Override
     public void start(Stage primaryStage) {
-        //String workingDir = System.getProperty("user.dir");
+       
+        buildMediaPlayer(primaryStage);
+    }
+    
+    private void buildMediaPlayer(Stage primaryStage){
+         //String workingDir = System.getProperty("user.dir");
+        //Ruta del fichero a abrir
+        //TODO Cambiar ruta dinamica
         final File f = new File("titanfall.mp4");
+        
+        final Media media = new Media(f.toURI().toString());
+        final MediaPlayer mediaPlayer = new MediaPlayer(media);
+        final MediaView mediaView = new MediaView(mediaPlayer);
+        
+        final DoubleProperty width = mediaView.fitWidthProperty();
+        final DoubleProperty height = mediaView.fitHeightProperty();
 
-        final Media m = new Media(f.toURI().toString());
-        final MediaPlayer mp = new MediaPlayer(m);
-        final MediaView mv = new MediaView(mp);
+        width.bind(Bindings.selectDouble(mediaView.sceneProperty(), "width"));
+        height.bind(Bindings.selectDouble(mediaView.sceneProperty(), "height"));
 
-        final DoubleProperty width = mv.fitWidthProperty();
-        final DoubleProperty height = mv.fitHeightProperty();
+        mediaView.setPreserveRatio(true);
 
-        width.bind(Bindings.selectDouble(mv.sceneProperty(), "width"));
-        height.bind(Bindings.selectDouble(mv.sceneProperty(), "height"));
-
-        mv.setPreserveRatio(true);
-
+        /**
+         * ESCENA
+         **/
         StackPane root = new StackPane();
-        root.getChildren().add(mv);
-
+        root.getChildren().add(mediaView);
+        Button play = new Button();
+        
+        root.getChildren().add(play);
+        
+        
         final Scene scene = new Scene(root, 960, 540);
         scene.setFill(Color.BLACK);
 
@@ -52,7 +70,26 @@ public class PReproductorJavaFx extends Application{
         primaryStage.setFullScreen(true);
         primaryStage.show();
 
-        mp.play();
-      }
+        //Instancia del objeto RVideo
+        RVideo video = new RVideo(mediaPlayer);
+        Thread thread = new Thread(video);
+        thread.start();
+    }
+    
 }   
+
+
+class RVideo implements Runnable{
+
+    MediaPlayer mediaPlayer;
+    
+    public RVideo (MediaPlayer mediaPlayer){
+        this.mediaPlayer = mediaPlayer;
+    }
+    
+    @Override
+    public void run() {
+         mediaPlayer.play();
+    }
+}
 
